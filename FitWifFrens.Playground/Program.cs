bfe5951208a24aa5b9a576add03fc2324de3d94c
Dylan McKendry <dylan.mckendry@outlook.com>
2024-05-04 04:00:55 +1000

    Adding in Playground project

---
+++ FitWifFrens.Playground/Program.cs
@@ -0,0 +1,23 @@
+﻿using FitWifFrens.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace FitWifFrens.Playground
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = Host.CreateApplicationBuilder(args);
+
+            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(connectionString));
+
+            builder.Services.AddHostedService<Service>();
+
+            var app = builder.Build();
+
+            app.Run();
+        }
+    }
+}
---
+++ FitWifFrens.Playground/Service.cs
@@ -0,0 +1,30 @@
+﻿using FitWifFrens.Data;
+
+namespace FitWifFrens.Playground
+{
+    public class Service : IHostedService
+    {
+        private readonly DataContext _dataContext;
+
+        public Service(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _dataContext.Commitments.Add(new Commitment
+            {
+                Id = Guid.NewGuid(),
+                ContractAddress = "6bb2344f-b6fb-4ac6-b08e-725f6ee35e9a"
+            });
+
+            await _dataContext.SaveChangesAsync();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
