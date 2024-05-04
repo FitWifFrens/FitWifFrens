ca8343732799acff84a1d7d8418ca244805fa2f9
Dylan McKendry <dylan.mckendry@outlook.com>
2024-05-05 07:58:22 +1000

    .

--- FitWifFrens.Playground/Service.cs
+++ FitWifFrens.Playground/Service.cs
@@ -39,7 +39,7 @@ namespace FitWifFrens.Playground
                 Image = "images/runner0.png",
                 Amount = 2,
                 Complete = true,
-                ContractAddress = "0x947384ef21BB443416383A7FFeF3f1C3543c19eD",
+                ContractAddress = "0xDF5B443589e6a6f395602Baa722e906EF0e9f0e2",
                 Providers = new List<CommitmentProvider>
                 {
                     new CommitmentProvider
@@ -49,6 +49,12 @@ namespace FitWifFrens.Playground
                 }
             });
 
+            // Fresh Contracts
+            // 0xE0c9a330050d361b6024f99092E65D52ccfcfCd8
+            // 0x7fAc91826D38CF8a70267848eE6f1017AB10423b
+            // 0x6decaF380123802B47C6DEE671461D7dd65aE235
+            // 0xd52e0a12ADF0e6Ba4b14D10449755245D1b3BA40
+            // 0xDF5B443589e6a6f395602Baa722e906EF0e9f0e2
 
             var newCommitmentId = Guid.NewGuid();
 
@@ -59,7 +65,7 @@ namespace FitWifFrens.Playground
                 Description = "Record 3 activities on Strava with a total time of 90 minutes",
                 Image = "images/runner1.png",
                 Amount = 5,
-                ContractAddress = "0x947384ef21BB443416383A7FFeF3f1C3543c19eD",
+                ContractAddress = "0x2b937ba128d275E16E7f26De7d8524C21d0BB7cA", // 1
                 Providers = new List<CommitmentProvider>
                 {
                     new CommitmentProvider
@@ -76,7 +82,7 @@ namespace FitWifFrens.Playground
                 Description = "Record 4 activities on Strava with a total time of 120 minutes",
                 Image = "images/runner2.png",
                 Amount = 10,
-                ContractAddress = "0x1d06111Db821c8728125e5480dD702486bd72A60",
+                ContractAddress = "0x6cC16743203C694f44Cf2698aa2537561832061e", // 2
                 Providers = new List<CommitmentProvider>
                 {
                     new CommitmentProvider
