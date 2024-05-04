ba720129116085d3bfa77b1ea081587e5a5f5ede
Dylan McKendry <dylan.mckendry@outlook.com>
2024-05-05 00:34:51 +1000

    Fixes to decimal places

--- FitWifFrens.Web.Client/Constants.cs
+++ FitWifFrens.Web.Client/Constants.cs
@@ -3,6 +3,7 @@
     public static class Constants
     {
         public static string TokenAddress = "0x50036df8bf8E077901eE419cFBAA7AcBf5AD9994";
-        public static long IncreaseAllowance = 10000;
+        public static long TokenDecimals = 100;
+        public static long IncreaseAllowance = 1000000;
     }
 }
