15dbf95e02b126dc6cea09ed42e53db4e90219cf
Dylan McKendry <dylan.mckendry@outlook.com>
2024-07-24 22:32:48 +1000

    .

--- FitWifFrens.Web/Background/StravaService.cs
+++ FitWifFrens.Web/Background/StravaService.cs
@@ -1,4 +1,5 @@
 ﻿using FitWifFrens.Data;
+using Microsoft.ApplicationInsights;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
@@ -10,129 +11,135 @@ namespace FitWifFrens.Web.Background
     {
         private readonly DataContext _dataContext;
         private readonly HttpClient _httpClient;
+        private readonly TelemetryClient _telemetryClient;
         private readonly ILogger<StravaService> _logger;
 
-        public StravaService(DataContext dataContext, IHttpClientFactory httpClientFactory, ILogger<StravaService> logger)
+        public StravaService(DataContext dataContext, IHttpClientFactory httpClientFactory, TelemetryClient telemetryClient, ILogger<StravaService> logger)
         {
             _dataContext = dataContext;
             _httpClient = httpClientFactory.CreateClient();
+            _telemetryClient = telemetryClient;
             _logger = logger;
         }
 
         public async Task UpdateProviderMetricValues(CancellationToken cancellationToken)
         {
-            _logger.LogWarning("UpdateProviderMetricValues");
-
-            var providerMetricValues = await _dataContext.ProviderMetricValues.Where(pmv => pmv.ProviderName == "Strava").ToListAsync(cancellationToken);
-
-            if (providerMetricValues.Any())
+            try
             {
-                foreach (var user in await _dataContext.Users.Include(u => u.Tokens).ToListAsync(cancellationToken))
+                var providerMetricValues = await _dataContext.ProviderMetricValues.Where(pmv => pmv.ProviderName == "Strava").ToListAsync(cancellationToken);
+
+                if (providerMetricValues.Any())
                 {
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    var tokens = user.Tokens.Where(t => t.LoginProvider == "Strava").ToList();
-
-                    if (tokens.Any())
+                    foreach (var user in await _dataContext.Users.Include(u => u.Tokens).ToListAsync(cancellationToken))
                     {
-                        var accessToken = tokens.Single(t => t.Name == "access_token");
-                        //var refreshToken = tokens.Single(t => t.Name == "refresh_token");
-                        ;
-                        using var request = new HttpRequestMessage(HttpMethod.Get, QueryHelpers.AddQueryString("https://www.strava.com/api/v3/athlete/activities", new Dictionary<string, string?>
+                        cancellationToken.ThrowIfCancellationRequested();
+
+                        var tokens = user.Tokens.Where(t => t.LoginProvider == "Strava").ToList();
+
+                        if (tokens.Any())
                         {
-                            { "after", DateTime.UtcNow.AddDays(-30).ToUnixTimeSeconds().ToString() },
-                            { "per_page", "200" }
-                        }));
-                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
+                            var accessToken = tokens.Single(t => t.Name == "access_token");
+                            //var refreshToken = tokens.Single(t => t.Name == "refresh_token");
 
-                        using var response = await _httpClient.SendAsync(request, cancellationToken);
-
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            ;
-                        }
-
-                        using var responseJson = JsonDocument.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
-
-                        foreach (var activityJson in responseJson.RootElement.EnumerateArray())
-                        {
-                            var activityTime = activityJson.GetProperty("start_date").GetDateTime();
-                            var activityMinutes = Math.Round(activityJson.GetProperty("elapsed_time").GetInt32() / 60.0, 2);
-
-                            var activityType = activityJson.GetProperty("type").GetString();
-
-                            if (activityType == "Run" || activityType == "VirtualRun")
+                            using var request = new HttpRequestMessage(HttpMethod.Get, QueryHelpers.AddQueryString("https://www.strava.com/api/v3/athlete/activities", new Dictionary<string, string?>
                             {
-                                var userProviderMetricValue = await _dataContext.UserProviderMetricValues
-                                    .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Strava" && upmv.MetricName == "Running" &&
-                                                                  upmv.MetricType == MetricType.Minutes && upmv.Time == activityTime, cancellationToken: cancellationToken);
+                                { "after", DateTime.UtcNow.AddDays(-30).ToUnixTimeSeconds().ToString() },
+                                { "per_page", "200" }
+                            }));
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
 
-                                if (userProviderMetricValue == null)
+                            using var response = await _httpClient.SendAsync(request, cancellationToken);
+
+                            if (!response.IsSuccessStatusCode)
+                            {
+                                ;
+                            }
+
+                            using var responseJson = JsonDocument.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
+
+                            foreach (var activityJson in responseJson.RootElement.EnumerateArray())
+                            {
+                                var activityTime = activityJson.GetProperty("start_date").GetDateTime();
+                                var activityMinutes = Math.Round(activityJson.GetProperty("elapsed_time").GetInt32() / 60.0, 2);
+
+                                var activityType = activityJson.GetProperty("type").GetString();
+
+                                if (activityType == "Run" || activityType == "VirtualRun")
                                 {
-                                    _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                    var userProviderMetricValue = await _dataContext.UserProviderMetricValues
+                                        .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Strava" && upmv.MetricName == "Running" &&
+                                                                      upmv.MetricType == MetricType.Minutes && upmv.Time == activityTime, cancellationToken: cancellationToken);
+
+                                    if (userProviderMetricValue == null)
                                     {
-                                        UserId = user.Id,
-                                        ProviderName = "Strava",
-                                        MetricName = "Running",
-                                        MetricType = MetricType.Minutes,
-                                        Time = activityTime,
-                                        Value = activityMinutes
-                                    });
+                                        _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                        {
+                                            UserId = user.Id,
+                                            ProviderName = "Strava",
+                                            MetricName = "Running",
+                                            MetricType = MetricType.Minutes,
+                                            Time = activityTime,
+                                            Value = activityMinutes
+                                        });
 
-                                    await _dataContext.SaveChangesAsync(cancellationToken);
-                                }
-                                else if (userProviderMetricValue.Value != activityMinutes)
-                                {
-                                    userProviderMetricValue.Value = activityMinutes;
-
-                                    _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
-
-                                    await _dataContext.SaveChangesAsync(cancellationToken);
-                                }
-                            }
-                            else if (activityType == "Ride" || activityType == "VirtualRide")
-                            {
-
-                            }
-                            else if (activityType == "Workout" || activityType == "Yoga")
-                            {
-
-                            }
-
-
-                            {
-                                var userProviderMetricValue = await _dataContext.UserProviderMetricValues
-                                    .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Strava" && upmv.MetricName == "Exercise" &&
-                                                                  upmv.MetricType == MetricType.Minutes && upmv.Time == activityTime, cancellationToken: cancellationToken);
-
-                                if (userProviderMetricValue == null)
-                                {
-                                    _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                        await _dataContext.SaveChangesAsync(cancellationToken);
+                                    }
+                                    else if (userProviderMetricValue.Value != activityMinutes)
                                     {
-                                        UserId = user.Id,
-                                        ProviderName = "Strava",
-                                        MetricName = "Exercise",
-                                        MetricType = MetricType.Minutes,
-                                        Time = activityTime,
-                                        Value = activityMinutes
-                                    });
+                                        userProviderMetricValue.Value = activityMinutes;
 
-                                    await _dataContext.SaveChangesAsync(cancellationToken);
+                                        _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
+
+                                        await _dataContext.SaveChangesAsync(cancellationToken);
+                                    }
                                 }
-                                else if (userProviderMetricValue.Value != activityMinutes)
+                                else if (activityType == "Ride" || activityType == "VirtualRide")
                                 {
-                                    userProviderMetricValue.Value = activityMinutes;
 
-                                    _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
+                                }
+                                else if (activityType == "Workout" || activityType == "Yoga")
+                                {
 
-                                    await _dataContext.SaveChangesAsync(cancellationToken);
+                                }
+
+                                {
+                                    var userProviderMetricValue = await _dataContext.UserProviderMetricValues
+                                        .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Strava" && upmv.MetricName == "Exercise" &&
+                                                                      upmv.MetricType == MetricType.Minutes && upmv.Time == activityTime, cancellationToken: cancellationToken);
+
+                                    if (userProviderMetricValue == null)
+                                    {
+                                        _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                        {
+                                            UserId = user.Id,
+                                            ProviderName = "Strava",
+                                            MetricName = "Exercise",
+                                            MetricType = MetricType.Minutes,
+                                            Time = activityTime,
+                                            Value = activityMinutes
+                                        });
+
+                                        await _dataContext.SaveChangesAsync(cancellationToken);
+                                    }
+                                    else if (userProviderMetricValue.Value != activityMinutes)
+                                    {
+                                        userProviderMetricValue.Value = activityMinutes;
+
+                                        _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
+
+                                        await _dataContext.SaveChangesAsync(cancellationToken);
+                                    }
                                 }
                             }
-
                         }
                     }
                 }
+            }
+            catch (Exception exception)
+            {
+                _telemetryClient.TrackException(exception);
+                throw;
             }
         }
     }
--- FitWifFrens.Web/Background/WithingsService.cs
+++ FitWifFrens.Web/Background/WithingsService.cs
@@ -1,4 +1,5 @@
 ﻿using FitWifFrens.Data;
+using Microsoft.ApplicationInsights;
 using Microsoft.EntityFrameworkCore;
 using System.Net.Http.Headers;
 using System.Text.Json;
@@ -9,96 +10,104 @@ namespace FitWifFrens.Web.Background
     {
         private readonly DataContext _dataContext;
         private readonly HttpClient _httpClient;
+        private readonly TelemetryClient _telemetryClient;
         private readonly ILogger<WithingsService> _logger;
 
-        public WithingsService(DataContext dataContext, IHttpClientFactory httpClientFactory, ILogger<WithingsService> logger)
+        public WithingsService(DataContext dataContext, IHttpClientFactory httpClientFactory, TelemetryClient telemetryClient, ILogger<WithingsService> logger)
         {
             _dataContext = dataContext;
             _httpClient = httpClientFactory.CreateClient();
+            _telemetryClient = telemetryClient;
             _logger = logger;
         }
 
         public async Task UpdateProviderMetricValues(CancellationToken cancellationToken)
         {
-            _logger.LogWarning("UpdateProviderMetricValues");
-
-            var providerMetricValues = await _dataContext.ProviderMetricValues.Where(pmv => pmv.ProviderName == "Withings").ToListAsync(cancellationToken);
-
-            if (providerMetricValues.Any())
+            try
             {
-                foreach (var user in await _dataContext.Users.Include(u => u.Tokens).ToListAsync(cancellationToken))
+                var providerMetricValues = await _dataContext.ProviderMetricValues.Where(pmv => pmv.ProviderName == "Withings").ToListAsync(cancellationToken);
+
+                if (providerMetricValues.Any())
                 {
-                    cancellationToken.ThrowIfCancellationRequested();
-
-                    var tokens = user.Tokens.Where(t => t.LoginProvider == "Withings").ToList();
-
-                    if (tokens.Any())
+                    foreach (var user in await _dataContext.Users.Include(u => u.Tokens).ToListAsync(cancellationToken))
                     {
-                        var accessToken = tokens.Single(t => t.Name == "access_token");
-                        //var refreshToken = tokens.Single(t => t.Name == "refresh_token");
+                        cancellationToken.ThrowIfCancellationRequested();
 
-                        using var request = new HttpRequestMessage(HttpMethod.Post, "https://wbsapi.withings.net/measure");
-                        request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
+                        var tokens = user.Tokens.Where(t => t.LoginProvider == "Withings").ToList();
+
+                        if (tokens.Any())
                         {
-                            { "action", "getmeas" },
-                            { "meastypes", "1" },
-                            { "startdate", DateTime.UtcNow.AddDays(-30).ToUnixTimeSeconds().ToString() },
-                        });
-                        request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                        request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
+                            var accessToken = tokens.Single(t => t.Name == "access_token");
+                            //var refreshToken = tokens.Single(t => t.Name == "refresh_token");
 
-                        using var response = await _httpClient.SendAsync(request, cancellationToken);
-
-                        if (!response.IsSuccessStatusCode)
-                        {
-                            ;
-                        }
-                        // TODO: "{\"status\":401,\"body\":{},\"error\":\"XRequestID: Not provided invalid_token: The access token provided is invalid\"}"
-                        using var responseJson = JsonDocument.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
-
-                        foreach (var measureGroupJson in responseJson.RootElement.GetProperty("body").GetProperty("measuregrps").EnumerateArray())
-                        {
-                            var measureGroupTime = DateTimeExs.FromUnixTimeSeconds(measureGroupJson.GetProperty("created").GetInt64(), DateTimeKind.Utc);
-
-                            foreach (var measureJson in measureGroupJson.GetProperty("measures").EnumerateArray())
+                            using var request = new HttpRequestMessage(HttpMethod.Post, "https://wbsapi.withings.net/measure");
+                            request.Content = new FormUrlEncodedContent(new Dictionary<string, string>
                             {
-                                var measureType = measureJson.GetProperty("type").GetInt32();
+                                { "action", "getmeas" },
+                                { "meastypes", "1" },
+                                { "startdate", DateTime.UtcNow.AddDays(-30).ToUnixTimeSeconds().ToString() },
+                            });
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken.Value);
 
-                                if (measureType == 1)
+                            using var response = await _httpClient.SendAsync(request, cancellationToken);
+
+                            if (!response.IsSuccessStatusCode)
+                            {
+                                ;
+                            }
+                            // TODO: "{\"status\":401,\"body\":{},\"error\":\"XRequestID: Not provided invalid_token: The access token provided is invalid\"}"
+                            using var responseJson = JsonDocument.Parse(await response.Content.ReadAsStringAsync(cancellationToken));
+
+                            foreach (var measureGroupJson in responseJson.RootElement.GetProperty("body").GetProperty("measuregrps").EnumerateArray())
+                            {
+                                var measureGroupTime = DateTimeExs.FromUnixTimeSeconds(measureGroupJson.GetProperty("created").GetInt64(), DateTimeKind.Utc);
+
+                                foreach (var measureJson in measureGroupJson.GetProperty("measures").EnumerateArray())
                                 {
-                                    var measureValue = Math.Round(measureJson.GetProperty("value").GetInt32() / 1000.0, 1);
+                                    var measureType = measureJson.GetProperty("type").GetInt32();
 
-                                    var userProviderMetricValue = await _dataContext.UserProviderMetricValues
-                                        .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Withings" && upmv.MetricName == "Weight" &&
-                                                                      upmv.MetricType == MetricType.Value && upmv.Time == measureGroupTime, cancellationToken: cancellationToken);
-
-                                    if (userProviderMetricValue == null)
+                                    if (measureType == 1)
                                     {
-                                        _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                        var measureValue = Math.Round(measureJson.GetProperty("value").GetInt32() / 1000.0, 1);
+
+                                        var userProviderMetricValue = await _dataContext.UserProviderMetricValues
+                                            .SingleOrDefaultAsync(upmv => upmv.UserId == user.Id && upmv.ProviderName == "Withings" && upmv.MetricName == "Weight" &&
+                                                                          upmv.MetricType == MetricType.Value && upmv.Time == measureGroupTime, cancellationToken: cancellationToken);
+
+                                        if (userProviderMetricValue == null)
                                         {
-                                            UserId = user.Id,
-                                            ProviderName = "Withings",
-                                            MetricName = "Weight",
-                                            MetricType = MetricType.Value,
-                                            Time = measureGroupTime,
-                                            Value = measureValue
-                                        });
+                                            _dataContext.UserProviderMetricValues.Add(new UserProviderMetricValue
+                                            {
+                                                UserId = user.Id,
+                                                ProviderName = "Withings",
+                                                MetricName = "Weight",
+                                                MetricType = MetricType.Value,
+                                                Time = measureGroupTime,
+                                                Value = measureValue
+                                            });
 
-                                        await _dataContext.SaveChangesAsync(cancellationToken);
-                                    }
-                                    else if (userProviderMetricValue.Value != measureValue)
-                                    {
-                                        userProviderMetricValue.Value = measureValue;
+                                            await _dataContext.SaveChangesAsync(cancellationToken);
+                                        }
+                                        else if (userProviderMetricValue.Value != measureValue)
+                                        {
+                                            userProviderMetricValue.Value = measureValue;
 
-                                        _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
+                                            _dataContext.Entry(userProviderMetricValue).State = EntityState.Modified;
 
-                                        await _dataContext.SaveChangesAsync(cancellationToken);
+                                            await _dataContext.SaveChangesAsync(cancellationToken);
+                                        }
                                     }
                                 }
                             }
                         }
                     }
                 }
+            }
+            catch (Exception exception)
+            {
+                _telemetryClient.TrackException(exception);
+                throw;
             }
         }
     }
