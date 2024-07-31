f092aaba5978a3f3efeae18307cc416917347c45
Dylan McKendry <dylan.mckendry@outlook.com>
2024-08-01 09:02:28 +1000

    .

--- FitWifFrens.Web/Controllers/DisplaysController.cs
+++ FitWifFrens.Web/Controllers/DisplaysController.cs
@@ -79,7 +79,7 @@ namespace FitWifFrens.Web.Controllers
 
             var metricsToShow = new List<(string MetricTitle, float TitleNudgeX, string MetricName, string ProviderName, MetricType MetricType)>
             {
-                new("\uf017", titleNudgeX1, "Exercise", "Strava", MetricType.Minutes), // f554 f004 f017
+                new("\uf017", titleNudgeX1, "Exercise", "Strava", MetricType.Minutes),
                 new("\uf70c", titleNudgeX2, "Running", "Strava", MetricType.Minutes),
                 new("\uf44b", titleNudgeX3, "Workout", "Strava", MetricType.Minutes),
                 new("\uf496", titleNudgeX4, "Weight", "Withings", MetricType.Value),
